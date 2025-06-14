9996141acb1419ce00ce44ddf2567960a43fa202
Wilter Quesada <wilterq@gmail.com>
2025-06-14 16:43:35 -0400

    Fix redirect from / to /book/

--- scheduler/urls.py
+++ scheduler/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
+from django.shortcuts import redirect
 from bookings import views
 
 urlpatterns = [
+    path('', lambda request: redirect('booking', permanent=False)),  # Redirect /
     path('admin/', admin.site.urls),
     path('book/', views.public_booking_view, name='booking'),
     path('thank-you/', views.thank_you_view, name='thank_you'),
