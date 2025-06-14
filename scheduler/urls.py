2ee61b605fb6b9dbb2f1822151cbba8a7f02f177
Wilter Quesada <wilterq@gmail.com>
2025-06-14 16:31:47 -0400

    Add project files with routes and views

--- scheduler/urls.py
+++ scheduler/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from bookings import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('bookings.urls')),  # ✅ This connects root URL to your app
+    path('book/', views.public_booking_view, name='booking'),
+    path('thank-you/', views.thank_you_view, name='thank_you'),
 ]
