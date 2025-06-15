5c9e70b7b484d0158df2bf84e150e47a7aa95fb7
Corvette052 <46474503+Corvette052@users.noreply.github.com>
2025-06-15 08:15:56 -0400

    Add thank you view and tests

--- bookings/tests.py
+++ bookings/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class ThankYouViewTests(TestCase):
+    def test_thank_you_view_renders_template(self):
+        url = reverse('thank_you')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'bookings/thank_you.html')
